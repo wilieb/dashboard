bb0d3e998aac449516a19b7e1e14427405e3f8a6
William Owalla <billyowala@gmail.com>
2022-11-25 13:54:11 +0300

    setting up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashComponent } from './dash/dash.component';
+import { NavComponent } from './dash/nav/nav.component';
+import { SideNavComponent } from './dash/side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashComponent,
+    NavComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule
